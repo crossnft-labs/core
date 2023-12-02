94e70896dbba5cdd5fdbeddc1e045aea7a769cd4
Gino <ginowine1@gmail.com>
2023-12-02 16:42:06 +0100

    create contract and constructor

---
+++ contracts/CrossNftSourceMinter.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import {LinkTokenInterface} from "@chainlink/contracts/src/v0.8/shared/interfaces/LinkTokenInterface.sol";
+import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
+import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
+import {Withdraw} from "./utils/Withdraw.sol";
+
+contract CrossNftSourceMinter is Withdraw {
+    enum PayFeesIn {
+        Native,
+        LINK
+    }
+    ddress immutable i_router;
+    address immutable i_link;
+
+    event MessageSent(bytes32 messageId);
+
+    constructor(address router, address link) {
+        i_router = router;
+        i_link = link;
+        LinkTokenInterface(i_link).approve(i_router, type(uint256).max);
+    }
+}
