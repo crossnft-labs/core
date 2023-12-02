c15ada77fc138fde9d0f020bab85ab5ec942e0e7
Gino <ginowine1@gmail.com>
2023-12-02 16:43:44 +0100

    create mint function on source chain

--- contracts/CrossNftSourceMinter.sol
+++ contracts/CrossNftSourceMinter.sol
@@ -21,4 +21,42 @@ contract CrossNftSourceMinter is Withdraw {
         i_link = link;
         LinkTokenInterface(i_link).approve(i_router, type(uint256).max);
     }
+
+    receive() external payable {}
+
+    function mint(
+        uint64 destinationChainSelector,
+        address receiver,
+        PayFeesIn payFeesIn
+    ) external {
+        Client.EVM2AnyMessage memory message = Client.EVM2AnyMessage({
+            receiver: abi.encode(receiver),
+            data: abi.encodeWithSignature("mint(address)", msg.sender),
+            tokenAmounts: new Client.EVMTokenAmount[](0),
+            extraArgs: "",
+            feeToken: payFeesIn == PayFeesIn.LINK ? i_link : address(0)
+        });
+
+        uint256 fee = IRouterClient(i_router).getFee(
+            destinationChainSelector,
+            message
+        );
+
+        bytes32 messageId;
+
+        if (payFeesIn == PayFeesIn.LINK) {
+            // LinkTokenInterface(i_link).approve(i_router, fee);
+            messageId = IRouterClient(i_router).ccipSend(
+                destinationChainSelector,
+                message
+            );
+        } else {
+            messageId = IRouterClient(i_router).ccipSend{value: fee}(
+                destinationChainSelector,
+                message
+            );
+        }
+
+        emit MessageSent(messageId);
+    }
 }
