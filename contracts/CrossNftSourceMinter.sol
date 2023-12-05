4016aedfe6f5eb7c8629a1564cc8c0f7b6edd91b
Gino <ginowine1@gmail.com>
2023-12-05 23:43:07 +0100

    create events, modifier, and errors

--- contracts/CrossNftSourceMinter.sol
+++ contracts/CrossNftSourceMinter.sol
@@ -11,10 +11,31 @@ contract CrossNftSourceMinter is Withdraw {
         Native,
         LINK
     }
-    ddress immutable i_router;
+    address immutable i_router;
     address immutable i_link;
 
-    event MessageSent(bytes32 messageId);
+    mapping(uint64 => bool) public whitelistedChains;
+
+    error NotEnoughBalance(uint256 currentBalance, uint256 calculatedFees); 
+    error DestinationChainNotWhitelisted(uint64 destinationChainSelector);
+    error NothingToWithdraw();
+
+    //event MessageSent(bytes32 messageId);
+
+    event TokensTransferred(
+        bytes32 indexed messageId, // The unique ID of the message.
+        uint64 indexed destinationChainSelector, // The chain selector of the destination chain.
+        address receiver, // The address of the receiver on the destination chain.
+        address token, // The token address that was transferred.
+        uint256 tokenAmount, // The token amount that was transferred.
+        uint256 fees // The fees paid for sending the message.
+    );
+
+    modifier onlyWhitelistedChain(uint64 _destinationChainSelector) {
+        if (!whitelistedChains[_destinationChainSelector])
+            revert DestinationChainNotWhitelisted(_destinationChainSelector);
+        _;
+    }
 
     constructor(address router, address link) {
         i_router = router;
